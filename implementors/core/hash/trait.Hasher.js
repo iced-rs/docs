965fe2972ec739cbc2a8d32b035c264f840c1e8c
hecrj <hecrj@users.noreply.github.com>
2022-05-03 18:07:41 +0000

    deploy: iced-rs/iced@02914e5e68d1fbaad53483cd32c74d9ac448d1eb

--- implementors/core/hash/trait.Hasher.js
+++ implementors/core/hash/trait.Hasher.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["iced_native"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"iced_native/struct.Hasher.html\" title=\"struct iced_native::Hasher\">Hasher</a>","synthetic":false,"types":["iced_native::hasher::Hasher"]}];
+implementors["iced_native"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"iced_native/struct.Hasher.html\" title=\"struct iced_native::Hasher\">Hasher</a>","synthetic":false,"types":["iced_native::hasher::Hasher"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
