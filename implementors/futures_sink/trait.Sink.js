6785c56135c05cacd0e179bbda506010fe9158c7
hecrj <hecrj@users.noreply.github.com>
2023-02-06 20:28:03 +0000

    deploy: iced-rs/iced@344578649ebcfe60d6a4010d743e68a423942e06

--- implementors/futures_sink/trait.Sink.js
+++ implementors/futures_sink/trait.Sink.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"iced_winit":[["impl&lt;Message:&nbsp;'static&gt; Sink&lt;Message&gt; for <a class=\"struct\" href=\"iced_winit/struct.Proxy.html\" title=\"struct iced_winit::Proxy\">Proxy</a>&lt;Message&gt;"]]
+"iced_winit":[["impl&lt;Message: 'static&gt; Sink&lt;Message&gt; for <a class=\"struct\" href=\"iced_winit/struct.Proxy.html\" title=\"struct iced_winit::Proxy\">Proxy</a>&lt;Message&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
