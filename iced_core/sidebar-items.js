c325312ac88dfba66d18643847e00ab9742cdca4
hecrj <hecrj@users.noreply.github.com>
2023-02-17 15:56:23 +0000

    deploy: iced-rs/iced@7dc1fb488ddbd12519571b51d75ae0c28875911d

--- iced_core/sidebar-items.js
+++ iced_core/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Background","ContentFit","Font","Length"],"macro":["color"],"mod":["alignment","keyboard","mouse","time"],"struct":["Color","Padding","Point","Rectangle","Size","Vector"]};
+window.SIDEBAR_ITEMS = {"enum":["Background","ContentFit","Font","Length"],"macro":["color"],"mod":["alignment","keyboard","mouse","time"],"struct":["Color","Padding","Pixels","Point","Rectangle","Size","Vector"]};
