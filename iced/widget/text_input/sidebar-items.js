346b9ca566d8c1289a5634e3fb1af40c3ffdd42e
hecrj <hecrj@users.noreply.github.com>
2023-04-11 04:21:56 +0000

    deploy: iced-rs/iced@ff24f9040c822fa7706087091e92ebee1e5211c5

--- iced/widget/text_input/sidebar-items.js
+++ iced/widget/text_input/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["focus","move_cursor_to","move_cursor_to_end","move_cursor_to_front","select_all"],"struct":["Appearance","Id"],"trait":["StyleSheet"],"type":["TextInput"]};
+window.SIDEBAR_ITEMS = {"enum":["Side"],"fn":["focus","move_cursor_to","move_cursor_to_end","move_cursor_to_front","select_all"],"struct":["Appearance","Icon","Id"],"trait":["StyleSheet"],"type":["TextInput"]};
--- iced_native/widget/text_input/sidebar-items.js
+++ iced_native/widget/text_input/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["draw","focus","layout","mouse_interaction","move_cursor_to","move_cursor_to_end","move_cursor_to_front","select_all","update"],"mod":["cursor"],"struct":["Appearance","Id","State","TextInput","Value"],"trait":["StyleSheet"]};
+window.SIDEBAR_ITEMS = {"enum":["Side"],"fn":["draw","focus","layout","mouse_interaction","move_cursor_to","move_cursor_to_end","move_cursor_to_front","select_all","update"],"mod":["cursor"],"struct":["Appearance","Icon","Id","State","TextInput","Value"],"trait":["StyleSheet"]};
