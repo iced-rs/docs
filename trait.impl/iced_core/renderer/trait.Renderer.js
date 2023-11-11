ef04d279871e598057d81847e41cc5958507b732
hecrj <hecrj@users.noreply.github.com>
2023-11-11 03:38:46 +0000

    deploy: iced-rs/iced@bb2f557d6a75850aed8e8689348f7a544b364bf6

--- trait.impl/iced_core/renderer/trait.Renderer.js
+++ trait.impl/iced_core/renderer/trait.Renderer.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "iced":[],
 "iced_core":[],
-"iced_graphics":[["impl&lt;B: <a class=\"trait\" href=\"iced_graphics/backend/trait.Backend.html\" title=\"trait iced_graphics::backend::Backend\">Backend</a>, T&gt; <a class=\"trait\" href=\"iced_core/renderer/trait.Renderer.html\" title=\"trait iced_core::renderer::Renderer\">Renderer</a> for <a class=\"struct\" href=\"iced_graphics/renderer/struct.Renderer.html\" title=\"struct iced_graphics::renderer::Renderer\">Renderer</a>&lt;B, T&gt;"]],
+"iced_graphics":[["impl&lt;B: <a class=\"trait\" href=\"iced_graphics/backend/trait.Backend.html\" title=\"trait iced_graphics::backend::Backend\">Backend</a>, T&gt; Renderer for <a class=\"struct\" href=\"iced_graphics/renderer/struct.Renderer.html\" title=\"struct iced_graphics::renderer::Renderer\">Renderer</a>&lt;B, T&gt;"]],
 "iced_renderer":[["impl&lt;T&gt; Renderer for <a class=\"enum\" href=\"iced_renderer/enum.Renderer.html\" title=\"enum iced_renderer::Renderer\">Renderer</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/iced_core/text/editor/trait.Editor.js
+++ trait.impl/iced_core/text/editor/trait.Editor.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "iced":[],
 "iced_core":[],
-"iced_graphics":[["impl <a class=\"trait\" href=\"iced_core/text/editor/trait.Editor.html\" title=\"trait iced_core::text::editor::Editor\">Editor</a> for <a class=\"struct\" href=\"iced_graphics/text/editor/struct.Editor.html\" title=\"struct iced_graphics::text::editor::Editor\">Editor</a>"]]
+"iced_graphics":[["impl Editor for <a class=\"struct\" href=\"iced_graphics/text/editor/struct.Editor.html\" title=\"struct iced_graphics::text::editor::Editor\">Editor</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
