1a286bc87b7a37459d0fd5304ed93e1440c52453
hecrj <hecrj@users.noreply.github.com>
2024-05-03 05:37:49 +0000

    deploy: iced-rs/iced@1cefe6be210cdae8c6769673e8d23c6781a988f1

---
+++ trait.impl/core/ops/arith/trait.Rem.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iced_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"iced_core/struct.Radians.html\" title=\"struct iced_core::Radians\">Radians</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
