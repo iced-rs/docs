d5bdcb025db81fc68d41b60f59fa1fdf59399c72
hecrj <hecrj@users.noreply.github.com>
2024-07-12 11:14:49 +0000

    deploy: iced-rs/iced@be06060117da061ad8cad94ab0830c06def6b147

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iced_runtime":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iced_runtime/task/struct.Handle.html\" title=\"struct iced_runtime::task::Handle\">Handle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
