418f629e5cab399af6ab5539b36f6a454d665f14
hecrj <hecrj@users.noreply.github.com>
2024-07-20 13:56:18 +0000

    deploy: iced-rs/iced@58f361d6806084c121c72e8752274c1cdf72fefc

--- trait.impl/iced_wgpu/primitive/trait.Renderer.js
+++ trait.impl/iced_wgpu/primitive/trait.Renderer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"iced_renderer":[["impl&lt;A, B&gt; Renderer for <a class=\"enum\" href=\"iced_renderer/fallback/enum.Renderer.html\" title=\"enum iced_renderer::fallback::Renderer\">Renderer</a>&lt;A, B&gt;<div class=\"where\">where\n    A: Renderer,\n    B: Renderer,</div>"]],
+"iced_renderer":[["impl&lt;A, B&gt; <a class=\"trait\" href=\"iced_wgpu/primitive/trait.Renderer.html\" title=\"trait iced_wgpu::primitive::Renderer\">Renderer</a> for <a class=\"enum\" href=\"iced_renderer/fallback/enum.Renderer.html\" title=\"enum iced_renderer::fallback::Renderer\">Renderer</a>&lt;A, B&gt;<div class=\"where\">where\n    A: <a class=\"trait\" href=\"iced_wgpu/primitive/trait.Renderer.html\" title=\"trait iced_wgpu::primitive::Renderer\">Renderer</a>,\n    B: <a class=\"trait\" href=\"iced_core/renderer/trait.Renderer.html\" title=\"trait iced_core::renderer::Renderer\">Renderer</a>,</div>"]],
 "iced_wgpu":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
