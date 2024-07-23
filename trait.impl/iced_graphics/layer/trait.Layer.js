54a5413d4e718c2bf7ec67505ee3340171cca71e
hecrj <hecrj@users.noreply.github.com>
2024-07-23 16:17:47 +0000

    deploy: iced-rs/iced@c47844ca2b0dd3efbcb0a850878867bbb2a59a6c

--- trait.impl/iced_graphics/layer/trait.Layer.js
+++ trait.impl/iced_graphics/layer/trait.Layer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "iced_tiny_skia":[["impl Layer for <a class=\"struct\" href=\"iced_tiny_skia/struct.Layer.html\" title=\"struct iced_tiny_skia::Layer\">Layer</a>"]],
-"iced_wgpu":[["impl <a class=\"trait\" href=\"iced_graphics/layer/trait.Layer.html\" title=\"trait iced_graphics::layer::Layer\">Layer</a> for <a class=\"struct\" href=\"iced_wgpu/layer/struct.Layer.html\" title=\"struct iced_wgpu::layer::Layer\">Layer</a>"]]
+"iced_wgpu":[["impl Layer for <a class=\"struct\" href=\"iced_wgpu/layer/struct.Layer.html\" title=\"struct iced_wgpu::layer::Layer\">Layer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
