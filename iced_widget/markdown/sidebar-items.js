70ac267da2fcd6edb332c4a502d1c1d2f9fa1610
hecrj <hecrj@users.noreply.github.com>
2024-08-21 00:36:31 +0000

    deploy: iced-rs/iced@55764b923e69afa8a92b4a6cbd34ee9ddbf8a03d

--- iced_widget/markdown/sidebar-items.js
+++ iced_widget/markdown/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["HeadingLevel","Item"],"fn":["parse","view"],"struct":["Settings","Url"]};
+window.SIDEBAR_ITEMS = {"enum":["HeadingLevel","Item"],"fn":["parse","view"],"struct":["Settings","Url"],"trait":["Catalog"]};
