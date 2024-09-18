bb55bf784bd62787a9d1787f4a22cbc8edd00963
hecrj <hecrj@users.noreply.github.com>
2024-09-18 01:16:49 +0000

    deploy: iced-rs/iced@f89744283167a1961fcff512ad48b0eb9b8fcaef

--- trait.impl/iced_core/clipboard/trait.Clipboard.js
+++ trait.impl/iced_core/clipboard/trait.Clipboard.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iced",[]],["iced_core",[]],["iced_winit",[["impl <a class=\"trait\" href=\"iced_core/clipboard/trait.Clipboard.html\" title=\"trait iced_core::clipboard::Clipboard\">Clipboard</a> for <a class=\"struct\" href=\"iced_winit/clipboard/struct.Clipboard.html\" title=\"struct iced_winit::clipboard::Clipboard\">Clipboard</a>"]]]]);
+    var implementors = Object.fromEntries([["iced",[]],["iced_core",[]],["iced_winit",[["impl Clipboard for <a class=\"struct\" href=\"iced_winit/clipboard/struct.Clipboard.html\" title=\"struct iced_winit::clipboard::Clipboard\">Clipboard</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11,17,297]}
+//{"start":57,"fragment_lengths":[11,17,176]}
