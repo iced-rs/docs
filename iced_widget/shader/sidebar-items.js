c36b806756ddbc5945d0b640935c466b87cbbc85
hecrj <hecrj@users.noreply.github.com>
2024-11-13 16:10:28 +0000

    deploy: iced-rs/iced@a11fcf8f2dde551335c6f34788393fa2e8f8a362

--- iced_widget/shader/sidebar-items.js
+++ iced_widget/shader/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Event"],"struct":["Shader","Storage","Viewport"],"trait":["Primitive","Program"]};
+window.SIDEBAR_ITEMS = {"struct":["Shader","Storage","Viewport"],"trait":["Primitive","Program"]};
