debdd1887fe6442748a6cc9e7fa113cd1097ec42
hecrj <hecrj@users.noreply.github.com>
2025-02-01 02:16:37 +0000

    deploy: iced-rs/iced@91f94f3b6e62bf05fb82b3c3bd999eff7b3fd339

--- iced_highlighter/sidebar-items.js
+++ iced_highlighter/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Theme"],"struct":["Highlight","Highlighter","Settings"]};
+window.SIDEBAR_ITEMS = {"enum":["Theme"],"struct":["Highlight","Highlighter","Settings","Stream"]};
