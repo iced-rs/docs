624412788e3aefa079522e2228aedb2a13306bb6
hecrj <hecrj@users.noreply.github.com>
2025-02-12 06:38:38 +0000

    deploy: iced-rs/iced@7c1123d6c9c51b54cc25cd88f7922658a42230fb

--- trait.impl/iced_graphics/compositor/trait.Default.js
+++ trait.impl/iced_graphics/compositor/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iced_graphics",[]],["iced_renderer",[["impl&lt;A, B&gt; <a class=\"trait\" href=\"iced_graphics/compositor/trait.Default.html\" title=\"trait iced_graphics::compositor::Default\">Default</a> for <a class=\"enum\" href=\"iced_renderer/fallback/enum.Renderer.html\" title=\"enum iced_renderer::fallback::Renderer\">Renderer</a>&lt;A, B&gt;<div class=\"where\">where\n    A: <a class=\"trait\" href=\"iced_graphics/compositor/trait.Default.html\" title=\"trait iced_graphics::compositor::Default\">Default</a>,\n    B: <a class=\"trait\" href=\"iced_graphics/compositor/trait.Default.html\" title=\"trait iced_graphics::compositor::Default\">Default</a>,</div>"]]],["iced_tiny_skia",[["impl Default for <a class=\"struct\" href=\"iced_tiny_skia/struct.Renderer.html\" title=\"struct iced_tiny_skia::Renderer\">Renderer</a>"]]],["iced_wgpu",[["impl <a class=\"trait\" href=\"iced_graphics/compositor/trait.Default.html\" title=\"trait iced_graphics::compositor::Default\">Default</a> for <a class=\"struct\" href=\"iced_wgpu/struct.Renderer.html\" title=\"struct iced_wgpu::Renderer\">Renderer</a>"]]]]);
+    var implementors = Object.fromEntries([["iced_graphics",[]],["iced_renderer",[["impl&lt;A, B&gt; Default for <a class=\"enum\" href=\"iced_renderer/fallback/enum.Renderer.html\" title=\"enum iced_renderer::fallback::Renderer\">Renderer</a>&lt;A, B&gt;<div class=\"where\">where\n    A: Default,\n    B: Default,</div>"]]],["iced_tiny_skia",[["impl Default for <a class=\"struct\" href=\"iced_tiny_skia/struct.Renderer.html\" title=\"struct iced_tiny_skia::Renderer\">Renderer</a>"]]],["iced_wgpu",[["impl Default for <a class=\"struct\" href=\"iced_wgpu/struct.Renderer.html\" title=\"struct iced_wgpu::Renderer\">Renderer</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[20,643,162,274]}
+//{"start":57,"fragment_lengths":[20,262,162,147]}
