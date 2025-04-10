4ca06a87a1d5d69dc79932479039107fd4be9c27
hecrj <hecrj@users.noreply.github.com>
2025-04-10 14:01:12 +0000

    deploy: iced-rs/iced@58e2d0cdcbac46a8bc0e1c19bf77bea82c2560d7

--- trait.impl/iced_core/widget/operation/text_input/trait.TextInput.js
+++ trait.impl/iced_core/widget/operation/text_input/trait.TextInput.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iced",[]],["iced_widget",[["impl&lt;P: <a class=\"trait\" href=\"iced_core/text/paragraph/trait.Paragraph.html\" title=\"trait iced_core::text::paragraph::Paragraph\">Paragraph</a>&gt; <a class=\"trait\" href=\"iced_core/widget/operation/text_input/trait.TextInput.html\" title=\"trait iced_core::widget::operation::text_input::TextInput\">TextInput</a> for <a class=\"struct\" href=\"iced_widget/text_input/struct.State.html\" title=\"struct iced_widget::text_input::State\">State</a>&lt;P&gt;"]]]]);
+    var implementors = Object.fromEntries([["iced",[]],["iced_widget",[["impl&lt;P: Paragraph&gt; TextInput for <a class=\"struct\" href=\"iced_widget/text_input/struct.State.html\" title=\"struct iced_widget::text_input::State\">State</a>&lt;P&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11,489]}
+//{"start":57,"fragment_lengths":[11,198]}
