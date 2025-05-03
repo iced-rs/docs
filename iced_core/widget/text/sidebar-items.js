3ef990fb52c2e81a7a9333da43d93af6b5ee1da1
hecrj <hecrj@users.noreply.github.com>
2025-05-03 03:55:39 +0000

    deploy: iced-rs/iced@f031b99196431692bf4084e38f7dd68771ab2d87

--- iced_core/widget/text/sidebar-items.js
+++ iced_core/widget/text/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["base","danger","default","draw","layout","primary","secondary","success"],"struct":["State","Style","Text"],"trait":["Catalog"],"type":["StyleFn"]};
+window.SIDEBAR_ITEMS = {"fn":["anchor","base","danger","default","draw","layout","primary","secondary","success"],"struct":["State","Style","Text"],"trait":["Catalog"],"type":["StyleFn"]};
