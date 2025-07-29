cdbbb4ddfbc283274a3bb1af586e5fdad23dd376
hecrj <hecrj@users.noreply.github.com>
2025-07-29 20:26:20 +0000

    deploy: iced-rs/iced@d897ccd49e08f7873880009688dd135fb80ae3d7

--- trait.impl/futures_sink/trait.Sink.js
+++ trait.impl/futures_sink/trait.Sink.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iced_winit",[["impl&lt;T: 'static&gt; Sink&lt;<a class=\"enum\" href=\"iced_runtime/enum.Action.html\" title=\"enum iced_runtime::Action\">Action</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"iced_winit/struct.Proxy.html\" title=\"struct iced_winit::Proxy\">Proxy</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["iced_winit",[["impl&lt;T: 'static&gt; Sink&lt;Action&lt;T&gt;&gt; for <a class=\"struct\" href=\"iced_winit/struct.Proxy.html\" title=\"struct iced_winit::Proxy\">Proxy</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[284]}
+//{"start":57,"fragment_lengths":[187]}
