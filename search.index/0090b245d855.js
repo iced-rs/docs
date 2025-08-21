15a304a019ae5a2de344a754f231ec297f2b5f3a
hecrj <hecrj@users.noreply.github.com>
2025-08-21 01:44:27 +0000

    deploy: iced-rs/iced@0e226f6f39b03e562cbf00548711a4ab16a4e741

---
+++ search.index/0090b245d855.js
@@ -0,0 +1,1 @@
+rn_("AQVhbmRsZQA6MAAAAQAAAAAAJQAQAAAALqIvomWlZqXuqu+q/6oAqwarB6s4qzmrRqtHq0mrSqtSrFOshKyFrIKtg62qrautLq4vrlGuUq5erl+uH68grzyvPa9Or0+vUq9Trw==")
---
+++ trait.impl/core/borrow/trait.BorrowMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iced_core",[["impl&lt;'a, Message, Theme, Renderer&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;dyn <a class=\"trait\" href=\"iced_core/widget/trait.Widget.html\" title=\"trait iced_core::widget::Widget\">Widget</a>&lt;Message, Theme, Renderer&gt; + 'a&gt; for &amp;mut <a class=\"struct\" href=\"iced_core/struct.Element.html\" title=\"struct iced_core::Element\">Element</a>&lt;'a, Message, Theme, Renderer&gt;"],["impl&lt;'a, Message, Theme, Renderer&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;dyn <a class=\"trait\" href=\"iced_core/widget/trait.Widget.html\" title=\"trait iced_core::widget::Widget\">Widget</a>&lt;Message, Theme, Renderer&gt; + 'a&gt; for <a class=\"struct\" href=\"iced_core/struct.Element.html\" title=\"struct iced_core::Element\">Element</a>&lt;'a, Message, Theme, Renderer&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1034]}
